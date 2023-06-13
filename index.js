3c13b8f153d0117549f0ca7bf560fe95dfb9f15c
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-06-13 14:48:48 +0100

    I think i am done

--- index.js
+++ index.js
@@ -9,34 +9,32 @@ var add = document.getElementById('add');
         else{
         let li= document.createElement('li')
         let button = document.createElement('button')
-        // var 
-        button.innerText = 'delete'
+        button.innerText = 'X'
         button.className = 'delete' 
         button.onclick = deleteNode
-        console.log(button)
+        // console.log(button)
         li.innerText = input.value + ' '
         li.appendChild(button)
+        li.className = 'li'
         // " <button class='delete'> Delete</button>"
         document.getElementById('lists').appendChild(li)
+        // localStorage.setItem('tasks',li)
+        // console.log(localStorage);
+        input.value = ''
         }
     }
     function deleteNode(e) {
         
         e.target.parentNode.remove()
     }
-    // for (let i = 0; i <  99; i++) {
-    //     // const element = array[i];
-    //     document.getElementsByClassName('delete')[i].addEventListener('click',(e)=>{
-    //         e.target.parentNode.remove()
-    //     })
-    // 
-    // }
-    // function delete(e) {
-    //     e.target.parentNode.remove
-    // }
-    for (let i = 0; i < 99; i++) {
-        document.getElementsByClassName('delete')[i].addEventListener('click',deleteNode())
 
-    }
+    // for (let i = 0; i < 99; i++) {
+    //     document.getElementsByClassName('delete')[i].addEventListener('click',deleteNode())
 
+    // }
     // pessit in localstorage
+   input.addEventListener('keydown',(e)=>{
+        if (e.code === 'Enter') {
+            addTasks()
+        }
+    })
